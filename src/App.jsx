4f1cea893e538afe1aab624bf4029c78627225f8
Yasin Cakal <whoisyasin@gmail.com>
2023-08-14 13:02:26 +0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,33 @@ import SearchItem from './SearchItem'
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist')) || []
-  )
+  const API_URL = 'http://localhost:3500/items'
+
+  const [items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items))
-  }, [items])
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error('Did not receive expected data')
+        const listItems = await response.json()
+        setItems(listItems)
+        setFetchError(null)
+      } catch (err) {
+        setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    setTimeout(() => {
+      fetchItems()
+    }, 2000)
+  }, [])
 
   const addItem = item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
@@ -54,13 +72,19 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
-        items={items.filter(item =>
-          item.item.toLowerCase().includes(search.toLocaleLowerCase())
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter(item =>
+              item.item.toLowerCase().includes(search.toLocaleLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      </main>
       <Footer length={items.length} />
     </div>
   )
